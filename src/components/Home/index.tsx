b8599e4b3f7d601b1deac24a63807742c859c7be
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-03-25 18:47:16 +0900

    set profile

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -30,6 +30,45 @@ export const Home: FC = () => {
                         </a>
                     </div>
                 </div>
+                <div className={SC.basic}>
+                    基本情報
+                    <div className={SC.description}>
+                        ネット上では 自由人、jiyuujin と名乗って生息しています。誕生日は1988年11月04日。大阪府豊中市生まれ。性別は男性。現在は関西圏（京都や大阪）を中心に活動しています。
+                    </div>
+                    <div className={SC.subtitle}>
+                        職業一覧
+                    </div>
+                    <div className={SC.description}>
+                        <ul>
+                            <li>フロントエンドエンジニア</li>
+                            <li>Webデベロッパ</li>
+                            <li>HRアドバイザ</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={SC.basic}>
+                    職歴
+                    <div className={SC.description}>
+                        自分のキャリアのスタートはiOSネイティブアプリのエンジニアとして。これまでのキャリアの中でiOSネイティブアプリエンジニアとして業務を通じ基礎的なコーディングを取得、Webを扱うサーバサイドで足りていないと言った環境も相まってjQueryを始めとしたCSS設計、フレームワークの使用など徐々にフロントエンドエンジニアという領域にも踏み込んでいきました。
+                    </div>
+                    <div className={SC.subtitle}>
+                        関連リンク一覧
+                    </div>
+                    <div className={SC.description}>
+                        <ul>
+                            <li>
+                                <a href="https://github.com/jiyuujin/Curriculum-Vitae">
+                                    職務経歴書
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://scrapbox.io/nekohack/skillset">
+                                    スキルセット
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import { Home } from '../components/Home'
+import { Footer } from '../components/Footer'
 import * as SC from './index.module.scss'
 
 export default class IndexPage extends React.Component<{}> {
@@ -9,6 +10,7 @@ export default class IndexPage extends React.Component<{}> {
             <Layout>
                 <div className={SC.container}>
                     <Home />
+                    <Footer />
                 </div>
             </Layout>
         )
