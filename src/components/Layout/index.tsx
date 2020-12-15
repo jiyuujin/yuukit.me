c090161d19b6b496f0c413fa0c8377fa00772e0f
jiyuujin <jiyuujinunite@gmail.com>
2020-12-15 20:26:03 +0900

    set optional type

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.module.scss'
 
-export default ({ children }: { children: React.ReactNode }) => (
+export default ({ children }: { children?: React.ReactNode }) => (
     <div>{children}</div>
 )
